294a9717d555d9a5013a2fb8b335336f4b90c8e0
huseyin <fzt.hsyns@gmail.com>
2023-03-28 22:22:13 +0300

    template

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -7,7 +8,26 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.green,
+        elevation: 0,
+        backgroundColor: tdBGColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.menu,
+              color: tdBlack,
+              size: 30,
+            ),
+            Container(
+              width: 30,
+              height: 30,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('assets/images/avatar.jpg'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
