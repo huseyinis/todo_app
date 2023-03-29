6010e2e85c4ba24e5e5300c9a1684c097b027a00
huseyin <fzt.hsyns@gmail.com>
2023-03-30 00:48:20 +0300

    ufak revize

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -10,13 +10,6 @@ class ToDo {
   });
 
   static List<ToDo> todoList() {
-    return [
-      ToDo(id: '01', todoText: 'Morning Exercise', isDone: true),
-      ToDo(id: '02', todoText: 'Buy Groceries', isDone: true),
-      ToDo(id: '03', todoText: 'Go Market'),
-      ToDo(id: '04', todoText: 'Morning Exercise'),
-      ToDo(id: '05', todoText: 'Make Dinner'),
-      ToDo(id: '06', todoText: 'Morning Exercise'),
-    ];
+    return [];
   }
 }
