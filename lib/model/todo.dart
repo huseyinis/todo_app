954b2f354a283d7cc4447688599a0e5500f936a7
huseyin <fzt.hsyns@gmail.com>
2023-03-30 15:50:40 +0300

    test commit

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -1,15 +1,1 @@
-class ToDo {
-  String? id;
-  String? todoText;
-  bool isDone;
 
-  ToDo({
-    required this.id,
-    required this.todoText,
-    this.isDone = false,
-  });
-
-  static List<ToDo> todoList() {
-    return [];
-  }
-}
