e111029e465ebb7fe39b829dfa524fbd5c100832
huseyin <fzt.hsyns@gmail.com>
2023-04-01 12:49:44 +0300

    m

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -1,1 +1,44 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
 
+import 'package:todo_app/model/todo.dart';
+
+class ToDoItem extends StatelessWidget {
+  final ToDo todo;
+  final Function onToDoChanged;
+  final Function onDeleteItem;
+  const ToDoItem({
+    Key? key,
+    required this.todo,
+    required this.onToDoChanged,
+    required this.onDeleteItem,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      child: ListTile(
+        onTap: () {
+          onToDoChanged(todo);
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        leading: Icon(todo.isDone ? Icons.check_box : Icons.check_box_outline_blank),
+        title: Text(
+          todo.todoText!,
+          style: TextStyle(
+            fontSize: 16,
+            decoration: todo.isDone ? TextDecoration.lineThrough : null,
+          ),
+        ),
+        trailing: IconButton(
+            onPressed: () {
+              onDeleteItem(todo.id);
+            },
+            icon: Icon(Icons.delete)),
+      ),
+    );
+  }
+}
